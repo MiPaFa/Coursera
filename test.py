b5e6027f989dfb05402efd01025044a43ad1404d
Familie Faas <michelfaas@iMac-van-Michel.local>
2018-12-08 15:02:46 +0100

    kleine oefening

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-n = 10
-while n>0:
-    print (n)
-    n=n-2
-print ('we zijn klaar')
+astr = input('geef nummer in:')
+try:
+    istr = int(astr)
+except:
+    istr = -1
+
+if istr > 0:
+    print(istr, 'is inderdaad een nummer')
+else:
+    print('ik zeg geef een NUMMER in! <', astr, '> is geen nummer')
+print ('done')
