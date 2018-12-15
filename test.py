4fb2f1f7f36ee61db8ca3cd6165bb839e0ecb5ed
Michel Faas <michelfaas@MacBook-Air-van-Michel.local>
2018-12-15 21:45:40 +0100

    laatste oefeningen

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-def greet(lang):
-    if lang == 'es':
-        print ('Hola')
-    elif lang == 'fr':
-        print ('Bonjour')
-    else:
-        print (Hello)
+
+prompt = 'geef een waarde in? '
+x = input(prompt)
+try:
+    fx = int(x)
+except:
+    print(x, 'is geen cijfer')
+    quit()
+if fx%2 == 0:
+    print (fx,'dit is een even getal')
+else:
+    print (fx,'dit is een oneven getal')
